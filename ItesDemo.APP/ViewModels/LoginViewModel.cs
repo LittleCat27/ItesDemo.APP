bc457264ee58b5c057e7971d6ad57de2395393d6
Joaquin Gregoire <GregoireJoaquin@gmail.com>
2023-09-22 17:54:58 -0300

    Agregar archivos de proyecto.

---
+++ ItesDemo.APP/ViewModels/LoginViewModel.cs
@@ -0,0 +1,97 @@
+﻿using ItesDemo.APP.Services;
+using ItesDemo.APP.Views;
+using System.Windows.Input;
+
+namespace ItesDemo.APP.ViewModels;
+
+public class LoginViewModel : BaseViewModel
+{
+    #region VARIABLES
+    private string usuario;
+    private string password;
+    private bool btnVisible;
+    #endregion
+
+    #region CONSTRUCTOR
+    public LoginViewModel()
+    {
+        #if DEBUG
+        Usuario = "Alex";
+        Password = "1234";
+#endif
+        BtnVisible = true;
+        // LoginCommand = new Command(async () => await Login());
+
+        LoginCommand = new(async () => await LoginAsync(),
+            () => !String.IsNullOrWhiteSpace(Usuario) && !String.IsNullOrWhiteSpace(Password));
+    }
+    #endregion
+
+    #region OBJETOS
+    public bool BtnVisible {
+        get { return btnVisible; }
+        set { SetProperty(ref btnVisible, value); }
+    }
+    public string Usuario
+    {
+        get { return usuario; }
+        set { SetProperty(ref usuario, value); }
+    }
+
+    public string Password
+    {
+        get { return password; }
+        set { SetProperty(ref password, value); }
+    }
+    #endregion
+
+    #region METODOS
+
+    private async Task LoginAsync()
+    {
+        if (!IsBusy)
+        {
+            IsBusy = true;
+            BtnVisible = false;
+
+            // asignamos objeto con datos del usuario-establecimiento logueados
+            if (usuario != string.Empty && password != string.Empty)
+            {
+
+                var apiClient = new ApiClient();
+
+                var response = await apiClient.ValidarLogin(usuario, password);
+
+                if (response != null)
+                {
+                    // guardo en memoria token para usar en consultas protegidas
+                    Configuration.AppConfiguration.Token = response.token;
+
+                    Application.Current.MainPage = new NavigationPage(new InicioPage());
+
+                    Usuario = string.Empty;
+                    Password = string.Empty;
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Atención", "Las credenciales ingresadas no son válidas", "Aceptar");
+                }
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Atención", "Las credenciales son Obligatorias. Verifique!", "Aceptar");
+            }
+
+            IsBusy = false;
+            BtnVisible = true;
+        }
+    }
+    #endregion
+
+    #region COMANDOS
+    public Command LoginCommand { get; set; }
+
+    #endregion
+}
+
+
---
+++ ItesDemo.APP/ViewModels/ProductoListaViewModel.cs
@@ -0,0 +1,91 @@
+﻿using ItesDemo.APP.Models;
+using ItesDemo.APP.Services;
+using ItesDemo.APP.Views;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ItesDemo.APP.ViewModels;
+
+public class ProductoListaViewModel : BaseViewModel
+{
+    #region VARIABLES
+    private bool isRefreshing;
+    private ProductoModel productoSeleccion;
+
+    private ObservableCollection<ProductoModel> productos = new ObservableCollection<ProductoModel>();
+    #endregion
+
+    #region CONSTRUCTOR
+    public ProductoListaViewModel()
+    {
+        Title = "Lista Productos";
+
+        Task.Run(async () => { await ConsultarApi(); }).Wait();
+
+        // ConsultarApi();
+
+        RefreshCommand = new Command(async () =>
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            await ConsultarApi();
+        });
+    }
+    #endregion
+
+    #region OBJETOS
+    public ObservableCollection<ProductoModel> Productos
+    {
+        get { return productos; }
+        set { SetProperty(ref productos, value); }
+    }
+
+    public ProductoModel ProductoSeleccion
+    {
+        get { return productoSeleccion; }
+        set { SetProperty(ref productoSeleccion, value); }
+    }
+
+    public bool IsRefreshing
+    {
+        get { return isRefreshing; }
+        set { SetProperty(ref isRefreshing, value); }
+    }
+    #endregion
+
+    #region METODOS
+    private async Task ConsultarApi()
+    {
+        IsBusy = IsRefreshing = true;
+
+        Productos.Clear();
+
+        var apiClient = new ApiClient();
+
+        var lista = await apiClient.ObtenerProductos();        
+
+        Productos = new ObservableCollection<ProductoModel>(lista);
+
+        IsBusy = IsRefreshing = false;
+    }
+
+    private void GoToCancelar()
+    {
+        Application.Current.MainPage.Navigation.PopAsync();
+    }
+
+    private void GoToDetail()
+    {
+        Application.Current.MainPage.Navigation.PushAsync(new ProductoDetallePage(productoSeleccion));
+    }
+    #endregion
+
+    #region COMANDOS
+    public ICommand GoToCancelarCommand => new Command(() => GoToCancelar());
+    public ICommand GoToDetailCommand => new Command(() => GoToDetail());    
+    public ICommand RefreshCommand { get; set; }
+    #endregion
+}
